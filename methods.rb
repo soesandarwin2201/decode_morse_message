d201f2dfef1dd78268ee3c824babf2cc255b189c
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-26 19:42:20 +0630

    Create the methods with pair-programming

--- methods.rb
+++ methods.rb
@@ -1,26 +1,53 @@
-"A": ".-",
-"B": "-...",
-"C": "-.-.",
-"D": "-..",
-"E": ".",
-"F": "..-.",
-"G": "--.",
-"H": "....",
-"I": "..",
-"J": ".---",
-"K": "-.-",
-"L": ".-..",
-"M": "--",
-"N": "-.",
-"O": "---",
-"P": ".--.",
-"Q": "--.-",
-"R": ".-.",
-"S": "...",
-"T": "-",
-"U": "..-",
-"V": "...-",
-"W": ".--",
-"X": "-..-",
-"Y": "-.--",
-"Z": "--.."
+def decode_char(character)
+  morse_code = { 
+  "A" => ".-",
+  "B" => "-...",
+  "C" => "-.-.",
+  "D" => "-..",
+  "E" => ".",
+  "F" => "..-.",
+  "G" => "--.",
+  "H"=> "....",
+  "I"=> "..",
+  "J"=> ".---",
+  "K"=> "-.-",
+  "L"=> ".-..",
+  "M"=> "--",
+  "N"=> "-.",
+  "O"=> "---",
+  "P"=> ".--.",
+  "Q"=> "--.-",
+  "R"=> ".-.",
+  "S"=> "...",
+  "T"=> "-",
+  "U"=> "..-", 
+  "V"=> "...-",
+  "W"=> ".--",
+  "X"=> "-..-",
+  "Y"=> "-.--",
+  "Z"=> "--.."
+ }
+
+  morse_code.each {|key, value| 
+ puts key if value == character}
+end
+
+decode_char("--..")
+
+def decode_word(word) 
+ word_split = word.split(" ")
+ word_array = word_split.map{ |word| decode_char(word) }
+ word_array.join('')
+end
+
+decode_word("-- -.--")
+
+
+def decode(sentence)
+  sentence_spiltted = sentence.split(" ")
+  sentence_array = sentence_spiltted.map{ |sentence| 
+   decode_word(sentence) }
+  sentence_array.join(" ")
+end
+
+decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
