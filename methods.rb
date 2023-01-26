52962de74128b02392f76a4404e72883aafb5edf
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-26 20:05:05 +0630

    Fix the linters

--- methods.rb
+++ methods.rb
@@ -1,48 +1,15 @@
 def decode_char(character)
-  morse_code = {
-    "A" => ".-",
-    "B" => "-...",
-    "C" => "-.-.",
-    "D" => "-..",
-    "E" => ".",
-    "F" => "..-.",
-    "G" => "--.",
-    "H" => "....",
-    "I" => "..",
-    "J" => ".---",
-    "K" => "-.-",
-    "L" => ".-..",
-    "M" => "--",
-    "N" => "-.",
-    "O" => "---",
-    "P" => ".--.",
-    "Q" => "--.-",
-    "R" => ".-.",
-    "S" => "...",
-    "T" => "-",
-    "U" => "..-",
-    "V" => "...-",
-    "W" => ".--",
-    "X" => "-..-",
-    "Y" => "-.--",
-    "Z" => "--.."
-  }
+  morse_code = { "A" => ".-", "B" => "-...", "C" => "-.-.", "D" => "-..", "E" => ".", "F" => "..-.", "G" => "--.",
+                 "H" => "....", "I" => "..", "J" => ".---", "K" => "-.-", "L" => ".-..", "M" => "--", "N" => "-.", "O" => "---", "P" => ".--.", "Q" => "--.-", "R" => ".-.", "S" => "...", "T" => "-", "U" => "..-", "V" => "...-", "W" => ".--", "X" => "-..-", "Y" => "-.--", "Z" => "--.." }
 
-  morse_code.each { |key, value|
-    puts key if value == character
-  }
+  morse_code.each { |key, value| return key if value == character }
 end
-
-decode_char("--..")
 
 def decode_word(word)
   word_split = word.split(" ")
   word_array = word_split.map { |word| decode_char(word) }
   word_array.join('')
 end
-
-decode_word("-- -.--")
-
 
 def decode(sentence)
   sentence_spiltted = sentence.split(" ")
@@ -52,4 +19,4 @@ def decode(sentence)
   sentence_array.join(" ")
 end
 
-decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
